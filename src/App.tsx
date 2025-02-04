9c701b4b223a37d314e9b2089454913074098fff
Benjamin Nyankson <benjamin.nyankson@amalitech.com>
2025-02-04 10:55:38 +0000

    Commit before version patch

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,25 @@
 import React, { useState } from "react";
 import AlertContainer from "./components/Alerts/stacked/AlertsContainer";
-import { v4 as uuidv4 } from "uuid";
 import { Alert } from "./components/Alerts";
+import { useAlerts } from "./hooks";
 
 const App: React.FC = () => {
-  const [alerts, setAlerts] = useState<
-    {
-      id: string;
-      variant: "error" | "warning" | "info" | "success";
-      type: "filled" | "outlined" | "standard";
-      position:
-        | "top"
-        | "top-right"
-        | "top-left"
-        | "center"
-        | "center-right"
-        | "center-left"
-        | "bottom"
-        | "bottom-right"
-        | "bottom-left";
-      title?: string;
-      description?: string;
-      autoDismiss?: boolean;
-      dismissTime?: number;
-    }[]
-  >([]);
 
-  // Function to add alerts
-  const addAlert = (alert: Omit<(typeof alerts)[number], "id">) => {
-    setAlerts((prev) => [...prev, { id: uuidv4(), ...alert }]);
-  };
-
-  // Function to remove alerts
-  const removeAlert = (id?: string) => {
-    setAlerts((prev) => prev.filter((alert) => alert.id !== id));
-  };
-
+  const {alerts, addAlert ,removeAlert} = useAlerts()
+  
   const [showAlert, setShowAlert] = useState(false);
   return (
     <div className="p-8">
       <AlertContainer alerts={alerts} removeAlert={removeAlert} />
-
+<div className="flex items-center justify-between">
+<p>hello</p>
+<p>hello</p>
+</div>
       <button onClick={() => setShowAlert(!showAlert)}>show</button>
       { showAlert && <Alert
         variant="success"
         type="filled"
-        position="top-right"
+        position="top-left"
         title="Success!"
         description="Your changes have been saved successfully."
         onClose={() => setShowAlert(false)}
